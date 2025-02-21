feb6b59e4848f4654caba777d02eeac9c5478ad9
acefa <acefa163@163.com>
2025-02-21 16:15:03 +0800

    更改页面元素

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 
 
 
@@ -6,11 +5,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex flex-col gap-2" >
+       
           <h1 className="text-4xl font-bold">TODO List</h1>
-          <input type="text" placeholder="Enter TODO" />
-          <button>Add</button>
-        </div>
+          
+          <input style={{width: '200px', height: '30px'}} type="text" placeholder="Enter TODO" />
+          <button style={{width: '100px', height: '30px'}}>添加</button>
+        
 
       </main>
       
